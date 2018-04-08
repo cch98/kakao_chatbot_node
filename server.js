820f568eaa4fae20391fbd155376bd983e108964
cch98 <changho9808@gmail.com>
2018-04-08 15:21:13 +0900

    connection test

---
+++ server.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+
+let bp = require('body-parser');
+
+app.use(bp.json);
+app.use(bp.urlencoded({extended:true}));
+
+app.get('/keyboard',function(req, res){
+    let keyboard = {
+        'type':'buttons',
+        'buttons':['시작하기']
+    };
+
+    res.set({
+        'content-type': 'application/json'
+    }).send(JSON.stringify(menu));
+
+});
+
+app.post('/message', function(req, res){
+    let ans = req.body;
+    console.dir(ans);
+
+});
+
+
+
+
+
+let port = 8000;
+app.listen(port, function(){
+    console.log('Listening in ' + port);
+});
